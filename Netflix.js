db108d39c3ce393b7b21c4317c73e90a149d7a35
DATABASESK <server1.2148652652@gmail.com>
2024-11-23 08:03:56 +0530

    Create Netflix.js

---
+++ Netflix.js
@@ -0,0 +1,8 @@
+import React from "react";
+import MovieGrid from "./MovieGrid";
+
+const Netflix = () => {
+  return <MovieGrid apiUrl="https://final-rj4x.onrender.com/netflix" />;
+};
+
+export default Netflix;
