2741d2b663ac754cc8ac814ef104a08118900a12
DATABASESK <server1.2148652652@gmail.com>
2024-11-23 08:32:41 +0530

    Delete Netflix.js

--- Netflix.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import MovieGrid from "./MovieGrid";
-
-const Netflix = () => {
-  return <MovieGrid apiUrl="https://final-rj4x.onrender.com/netflix" />;
-};
-
-export default Netflix;
